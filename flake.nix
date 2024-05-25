81f17e7bb76e85b2e1291c6fea67cbe118924c41
Sebastian Zawadzki <sebastian@zawadzki.tech>
2024-05-25 18:35:15 +0200

    Base machine to build isos

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }@inputs: {
+
+    nixosConfigurations = {
+      iso = nixpkgs.lib.nixosSystem {
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+          ./configuration.nix
+        ];
+        specialArgs = { inherit inputs; };
+      };
+    };
+  };
+}
