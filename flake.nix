a75755f49be6370a38ee44ebf8bab47b9249d1be
Sebastian Zawadzki <sebastian@zawadzki.tech>
2024-09-22 22:20:07 +0200

    flake parts

--- flake.nix
+++ flake.nix
@@ -1,53 +1,54 @@
 {
-  description = "A very basic flake";
+  outputs = inputs@{ self, flake-parts, nixos-generators, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      flake = {
+        nixosModules.vm = { config, ... }: {
+          imports = [ nixos-generators.nixosModules.all-formats ];
+          nixpkgs.hostPlatform = "aarch64-linux";
+        };
+
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+          modules = [ ./configuration.nix self.nixosModules.vm ];
+          specialArgs = {
+            inherit inputs;
+            hostName = "nixos";
+            diskSize = "16384";
+          };
+        };
+
+        nixosConfigurations.postgres = nixpkgs.lib.nixosSystem {
+          modules =
+            [ ./configuration.nix ./postgresql.nix self.nixosModules.vm ];
+          specialArgs = {
+            inherit inputs;
+            hostName = "postgres";
+            diskSize = "16384";
+          };
+        };
+
+      };
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ inputs.nil.packages.${system}.nil ];
+        };
+      };
+    };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nil = {
       url = "github:oxalica/nil";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-
-  outputs = { self, nixos-generators, nixpkgs, ... }@inputs: {
-    nixosModules.vm = { config, ... }: {
-      imports = [ nixos-generators.nixosModules.all-formats ];
-      nixpkgs.hostPlatform = "aarch64-linux";
-    };
-
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix self.nixosModules.vm ];
-      specialArgs = {
-        inherit inputs;
-        hostName = "nixos";
-        diskSize = "16384";
-      };
-    };
-
-    nixosConfigurations.postgres = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ./postgresql.nix self.nixosModules.vm ];
-      specialArgs = {
-        inherit inputs;
-        hostName = "postgres";
-        diskSize = "16384";
-      };
-    };
-
-    devShells.aarch64-darwin.default =
-      let pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-      in pkgs.mkShell {
-        packages = with pkgs; [ inputs.nil.packages.${system}.nil ];
-      };
-
-    devShells.x86_64-linux.default =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.mkShell {
-        packages = with pkgs; [ inputs.nil.packages.${system}.nil ];
-      };
-
   };
 }
