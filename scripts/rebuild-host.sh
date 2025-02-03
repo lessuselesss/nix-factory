86f82394270b19eecd9a2c4b10a70ae406d7fe72
Sebastian Zawadzki <sebastian@zawadzki.tech>
2025-02-03 22:03:52 +0100

    rebuild host now takes IP from utmctl

--- scripts/rebuild-host.sh
+++ scripts/rebuild-host.sh
@@ -8,9 +8,9 @@ if [[ "${TRACE-0}" == "1" ]]; then
     set -o xtrace
 fi
 
-if [[ "${1-}" =~ ^-*h(elp)?$ ]] || [[ $# -lt 2 ]]; then
-    echo 'Usage: ./rebuild-host.sh NAME IP
-    Rebuild host at IP address remotely with NAME flake configuration.
+if [[ "${1-}" =~ ^-*h(elp)?$ ]] || [[ $# -eq 0 ]]; then
+    echo 'Usage: ./rebuild-host.sh MACHINE [NAME]
+    Rebuild host MACHINE remotely based on flake.nix configuration. If more than one machine was built from the same image, or the machine has different name, then optionally specify NAME to target the correct machine.
 '
     exit
 fi
@@ -18,7 +18,9 @@ fi
 cd "$(git rev-parse --show-toplevel)"
 
 main() {
-    nixos-rebuild switch --flake .#"$1" --target-host nixos@"$2" --build-host nixos@"$2" --fast --use-remote-sudo
+    IP=$(utmctl ip-address "${!#}" 2>/dev/null | grep '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+    export NIX_SSHOPTS='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+    nixos-rebuild switch --flake .#"$1" --target-host nixos@"$IP" --build-host nixos@"$IP" --fast --use-remote-sudo
 }
 
 main "$@"
